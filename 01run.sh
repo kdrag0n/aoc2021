20d70d65c4b245c5dc5491e8fb5643600fb36825
Danny Lin <danny@kdrag0n.dev>
2021-11-30 21:15:12 -0800

    run: Update for 2021

--- 01run.sh
+++ 01run.sh
@@ -7,12 +7,12 @@ cd "$(dirname "$0")"
 num="$(echo "$1" | tr -d 't')"
 problem="${2:-}"
 
-if [ ! -f in/$num ]; then
+if [[ ! -f in/$num ]]; then
     echo "Download input"
-    curl -H "Cookie: session=$(cat .token)" "https://adventofcode.com/2020/day/$num/input" | tee in/$num
+    curl -H "Cookie: session=$(cat .token)" "https://adventofcode.com/2021/day/$num/input" | tee in/$num
 fi
 
-if [ "x$problem" = "x" ]; then
+if [[ -z "$problem" ]]; then
     ./day$num.py in/$num
 else
     ./day${num}-2.py in/$num
