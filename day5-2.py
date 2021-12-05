9e5cd15dbec0d93703e269675979e6142c1555ed
Danny Lin <danny@kdrag0n.dev>
2021-12-04 22:21:17 -0800

    day5-2: Simplify swapping

--- day5-2.py
+++ day5-2.py
@@ -44,17 +44,13 @@ for (x1, y1), (x2, y2) in lines:
     print('DRAW', (x1, y1), (x2, y2))
     if x1 == x2:
         if y1 > y2:
-            tmp = y1
-            y1 = y2
-            y2 = tmp
+            y1, y2 = y2, y1
         for y in range(y1, y2 + 1):
             print('  fill', x1, y)
             grid[y][x1] += 1
     elif y1 == y2:
         if x1 > x2:
-            tmp = x1
-            x1 = x2
-            x2 = tmp
+            x1, x2 = x2, x1
         for x in range(x1, x2 + 1):
             print('  fill', x, y1)
             grid[y1][x] += 1
