1e4a66a79aabc8cc35d97523ef43f1cea79681e7
Danny Lin <danny@kdrag0n.dev>
2021-12-04 23:46:17 -0800

    run: Create empty sample files

--- 01run.sh
+++ 01run.sh
@@ -7,10 +7,15 @@ cd "$(dirname "$0")"
 num="$(echo "$1" | tr -d 't')"
 problem="${2:-}"
 
+mkdir -p in
+mkdir -p samples
+
 if [[ ! -f in/$num ]]; then
     echo "Download input"
     curl -H "Cookie: session=$(cat .token)" "https://adventofcode.com/2021/day/$num/input" | tee in/$num
 fi
+
+touch samples/$num
 
 if [[ -z "$problem" ]]; then
     ./day$num.py in/$num
