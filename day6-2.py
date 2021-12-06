27cf5c6618dd82223f112ff6446b1c6d217c34f8
Danny Lin <danny@kdrag0n.dev>
2021-12-05 21:37:59 -0800

    Add solutions for day 6

--- day6-2.py
+++ day6-2.py
@@ -12,19 +12,23 @@ with open(sys.argv[1], 'r') as f:
 in_nums = [1,1,1,1,2,1,1,4,1,4,3,1,1,1,1,1,1,1,1,4,1,3,1,1,1,5,1,3,1,4,1,2,1,1,5,1,1,1,1,1,1,1,1,1,1,3,4,1,5,1,1,1,1,1,1,1,1,1,3,1,4,1,1,1,1,3,5,1,1,2,1,1,1,1,4,4,1,1,1,4,1,1,4,2,4,4,5,1,1,1,1,2,3,1,1,4,1,5,1,1,1,3,1,1,1,1,5,5,1,2,2,2,2,1,1,2,1,1,1,1,1,3,1,1,1,2,3,1,5,1,1,1,2,2,1,1,1,1,1,3,2,1,1,1,4,3,1,1,4,1,5,4,1,4,1,1,1,1,1,1,1,1,1,1,2,2,4,5,1,1,1,1,5,4,1,3,1,1,1,1,4,3,3,3,1,2,3,1,1,1,1,1,1,1,1,2,1,1,1,5,1,3,1,4,3,1,3,1,5,1,1,1,1,3,1,5,1,2,4,1,1,4,1,4,4,2,1,2,1,3,3,1,4,4,1,1,3,4,1,1,1,2,5,2,5,1,1,1,4,1,1,1,1,1,1,3,1,5,1,2,1,1,1,1,1,4,4,1,1,1,5,1,1,5,1,2,1,5,1,1,1,1,1,1,1,1,1,1,1,1,3,2,4,1,1,2,1,1,3,2]
 #in_nums = [3,4,3,1,2]
 
+vals = {i: sum((1 if x == i else 0) for x in in_nums) for i in range(9)}
+
 total = 0
 result = 0
 other = 0
 
-for day in range(80):
-    add_fs = []
-    for fi, count in enumerate(in_nums):
-        count -= 1
-        if count < 0:
-            count = 6
-            add_fs += [8]
-        in_nums[fi] = count
-    in_nums += add_fs
-    print('Day', day+1, ':', in_nums)
+days = 256
+for day in range(days):
+    add_fs = 0
+    for bucket, count in list(vals.items()):
+        new_bucket = bucket - 1
+        if new_bucket < 0:
+            new_bucket = 6
+            add_fs += count
+        vals[bucket] -= count
+        vals[new_bucket] += count
+    vals[8] += add_fs
+    print('Day', day+1, ':', vals)
 
-print(len(in_nums))
+print(sum(x for x in vals.values()))
