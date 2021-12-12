34840e191cf178c471051bc22154e0a644b2c482
Danny Lin <danny@kdrag0n.dev>
2021-12-12 01:18:02 -0800

    day12-2: Optimize small cave logic

--- day12-2.cpp
+++ day12-2.cpp
@@ -12,43 +12,28 @@ static inline bool is_small(const string& node) {
     return islower(node[0]) && node != "start" && node != "end";
 }
 
-static void dfs(vector<int>& node_counts, int& res_paths, int last_node) {
-    if (last_node == end_node) {
-        res_paths++;
-        if (res_paths % 1000 == 0) {
-            //cout << res_paths << '\n';
-        }
-        return;
-    }
-
+static void dfs(vector<int>& node_counts, int& res_paths, int last_node = start_node, bool used2 = false) {
     for (auto next : graph[last_node]) {
+        auto next_used2 = used2;
         if (next == start_node) {
             continue;
-        }
-
-        bool used2 = false;
-        bool bad = false;
-        for (int node = 0; node < node_counts.size(); node++) {
-            if (!small_nodes[node]) {
-                continue;
+        } else if (next == end_node) {
+            res_paths++;
+            if (res_paths % 1000 == 0) {
+                //cout << res_paths << '\n';
             }
-            auto ct = node_counts[node];
-
-            if (ct <= 1) {
-                continue;
-            } else if (ct == 2 && !used2) {
-                used2 = true;
+            continue;
+        } else if (small_nodes[next] && node_counts[next] >= 1) {
+            if (!next_used2) {
+                next_used2 = true;
             } else {
-                bad = true;
-                break;
+                continue;
             }
         }
 
-        if (!bad) {
-            vector<int> new_counts = node_counts;
-            new_counts[next] += 1;
-            dfs(new_counts, res_paths, next);
-        }
+        vector<int> new_counts = node_counts;
+        new_counts[next] += 1;
+        dfs(new_counts, res_paths, next, next_used2);
     }
 }
 
@@ -88,7 +73,7 @@ int main(int argc, char **argv) {
 
     int res_paths = 0;
     vector<int> start_counts(node_map.size());
-    dfs(start_counts, res_paths, start_node);
+    dfs(start_counts, res_paths);
 
     cout << '\n' << res_paths << '\n';
     return 0;
