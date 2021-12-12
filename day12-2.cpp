0c868d50604616c7e020cca29e7c330725bdc7b3
Danny Lin <danny@kdrag0n.dev>
2021-12-12 00:37:23 -0800

    day12-2: Replace maps and sets with vectors

--- day12-2.cpp
+++ day12-2.cpp
@@ -2,27 +2,18 @@
 
 using namespace std;
 
-static unordered_map<int, vector<int>> graph;
-static set<int> small_nodes;
+static vector<vector<int>> graph;
+static vector<bool> small_nodes;
 
-static inline int hash_node(const string& node) {
-    int hash = 17;
-    for (char ch : node) {
-        hash *= 31;
-        hash += ch;
-    }
-    return hash;
-}
-
-static auto start_hash = hash_node("start");
-static auto end_hash = hash_node("end");
+static constexpr int start_node = 0;
+static constexpr int end_node = 1;
 
 static inline bool is_small(const string& node) {
     return islower(node[0]) && node != "start" && node != "end";
 }
 
-static void dfs(unordered_map<int, int>& node_counts, int& res_paths, int last_node) {
-    if (last_node == end_hash) {
+static void dfs(vector<int>& node_counts, int& res_paths, int last_node) {
+    if (last_node == end_node) {
         res_paths++;
         if (res_paths % 1000 == 0) {
             //cout << res_paths << '\n';
@@ -31,16 +22,17 @@ static void dfs(unordered_map<int, int>& node_counts, int& res_paths, int last_n
     }
 
     for (auto next : graph[last_node]) {
-        if (next == start_hash) {
+        if (next == start_node) {
             continue;
         }
 
         bool used2 = false;
         bool bad = false;
-        for (auto& [node, ct] : node_counts) {
-            if (!small_nodes.count(node)) {
+        for (int node = 0; node < node_counts.size(); node++) {
+            if (!small_nodes[node]) {
                 continue;
             }
+            auto ct = node_counts[node];
 
             if (ct <= 1) {
                 continue;
@@ -53,7 +45,7 @@ static void dfs(unordered_map<int, int>& node_counts, int& res_paths, int last_n
         }
 
         if (!bad) {
-            unordered_map<int, int> new_counts = node_counts;
+            vector<int> new_counts = node_counts;
             new_counts[next] += 1;
             dfs(new_counts, res_paths, next);
         }
@@ -62,6 +54,10 @@ static void dfs(unordered_map<int, int>& node_counts, int& res_paths, int last_n
 
 int main(int argc, char **argv) {
     freopen(argv[1], "r", stdin);
+
+    unordered_map<string, int> node_map;
+    node_map["start"] = start_node;
+    node_map["end"] = end_node;
 
     while (!cin.eof()) {
         string line;
@@ -72,33 +68,27 @@ int main(int argc, char **argv) {
 
         auto frm = line.substr(0, line.find("-"));
         auto to = line.substr(line.find("-") + 1, line.length());
-        cout << frm << ' ' << to << '\n';
+        //cout << frm << ' ' << to << '\n';
 
-        auto frm_hash = hash_node(frm);
-        auto to_hash = hash_node(to);
-        if (graph.count(frm_hash) == 0) {
-            graph.emplace(frm_hash, vector<int>{});
-        }
-        graph[frm_hash].push_back(to_hash);
-        if (graph.count(to_hash) == 0) {
-            graph.emplace(to_hash, vector<int>{});
-        }
-        graph[to_hash].push_back(frm_hash);
+        auto from_node = node_map.count(frm) ? node_map[frm] : node_map[frm] = node_map.size();
+        auto to_node = node_map.count(to) ? node_map[to] : node_map[to] = node_map.size();
+        graph.resize(node_map.size());
+        small_nodes.resize(node_map.size());
+
+        graph[from_node].push_back(to_node);
+        graph[to_node].push_back(from_node);
 
         if (is_small(frm)) {
-            small_nodes.insert(frm_hash);
+            small_nodes[from_node] = true;
         }
         if (is_small(to)) {
-            small_nodes.insert(to_hash);
+            small_nodes[to_node] = true;
         }
     }
 
     int res_paths = 0;
-    unordered_map<int, int> start_counts;
-    for (auto& [node, neighbors] : graph) {
-        start_counts[node] = 0;
-    }
-    dfs(start_counts, res_paths, start_hash);
+    vector<int> start_counts(node_map.size());
+    dfs(start_counts, res_paths, start_node);
 
     cout << '\n' << res_paths << '\n';
     return 0;
