cb093a4e42a814733626f861d6f24ce6d5f04f9d
Danny Lin <danny@kdrag0n.dev>
2021-12-12 21:23:34 -0800

    Add solutions for day 13

--- day13-2.py
+++ day13-2.py
@@ -15,7 +15,8 @@ total = 0
 result = 0
 other = 0
 
-grid = [[False] * 2000 for i in range(2000)]
+SIZE = 2000
+grid = [[False] * SIZE for i in range(SIZE)]
 folds = []
 while True:
     for l in file_lines:
@@ -39,41 +40,37 @@ def print_grid():
     for r in grid:
         print(''.join(('#' if c else '.') for c in r))
 
+width = SIZE
+height = SIZE
 # print_grid()
 print(folds)
 for axis, pos in folds:
     print(axis, pos)
     if axis == 'x':
-        for x2 in range(pos + 1, 2000):
-            for y in range(2000):
+        for x2 in range(pos + 1, width):
+            for y in range(height):
                 if not grid[y][x2]:
                     continue
                 refp = pos - (x2 - (pos))
-                if refp < 0:
-                    continue
                 grid[y][refp] = True
                 grid[y][x2] = False
+                width = pos + 1
     elif axis == 'y':
-        for y2 in range(pos + 1, 2000):
-            for x in range(2000):
+        for y2 in range(pos + 1, height):
+            for x in range(width):
                 if not grid[y2][x]:
                     continue
                 refp = pos - (y2 - (pos))
                 # print(x, y2)
                 # print('refp', refp)
-                if refp < 0:
-                    continue
                 grid[refp][x] = True
                 grid[y2][x] = False
+                height = pos + 1
     # print_grid()
-    break
 
-x = 0
-for r in grid:
-    for c in r:
-        if c:
-            x += 1
-print(x)
+for r in grid[:height]:
+    print(''.join(('#' if c else '.') for c in r[:width]))
+
 print(f'Total: {total}')
 print(f'Result: {result}')
 print(f'Other: {other}')
